1d77daad2c1d821b60b9a61923da374b9a634901
Dzenkinstone <kozlovdanil6@gmail.com>
2023-06-01 11:23:45 +0300

    asd

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
+  console.log(err);
   const { status = 500, message = "Iternal server error" } = err;
   res.status(status).json({ message });
 });
